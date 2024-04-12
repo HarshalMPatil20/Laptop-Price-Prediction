3121e16e0a4f0ce1575b21ff6ef4a93fc0b1f75d
Harshal Patil <harshalmpatil210@gmail.com>
2024-04-12 18:01:10 +0530

    Running Bug fix

--- ML PRO.2/app.py
+++ ML PRO.2/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import numpy as np
 import base64
+import streamlit as st
+import pickle
 
 with open("style.css") as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
@@ -42,15 +44,15 @@ def add_bg_to_sidebar(image_file):
         """,
         unsafe_allow_html=True
     )
-
-# Set background image for the sidebar
 add_bg_to_sidebar('Login_bg.jpg')    
 
  
 
 # import the model
 pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
+
+file='df.pkl'
+df = pickle.load(open(file,'rb'))
 
 st.title("Laptop price Predictor")
 
@@ -113,6 +115,7 @@ if st.button('Predict Price'):
 
 
 with st.sidebar:
+    st.title("Author : Harshal Patil")
     st.title("What Factors Affect Laptop Computer Prices? ")
     st.text_area("Several different factors can affect laptop computer prices. These factors include the brand of computer and the number of options and add-ons included in the computer package. In addition, the amount of memory and the speed of the processor can also affect pricing. Though less common, some consumers spend additional money to purchase a computer based on the overall “look” and design of the system. In many cases, name brand computers are more expensive than generic versions. This price increase often has more to do with name recognition than any actual superiority of the product. One major difference between name brand and generic systems is that in most cases, name brand computers offer better warranties than generic versions. Having the option of returning a computer that is malfunctioning is often enough of an incentive to encourage many consumers to spend more money. Functionality is an important factor in determining laptop computer prices. A computer with more memory often performs better for a longer time than a computer with less memory. In addition, hard drive space is also crucial, and the size of the hard drive usually affects pricing. Many consumers may also look for digital video drivers and other types of recording devices that may affect the laptop computer prices. Most computers come with some software pre-installed. In most cases, the more software that is installed on a computer, the more expensive it is. This is especially true if the installed programs are from well-established and recognizable software publishers. Those considering purchasing a new laptop computer should be aware that many of the pre-installed programs may be trial versions only, and will expire within a certain time period. In order to keep the programs, a code will need to be purchased, and then a permanent version of the software can be downloaded. -Many consumers who are purchasing a new computer are buying an entire package. In addition to the computer itself, these systems typically include a monitor, keyboard, and mouse. Some packages may even include a printer or digital camera. The number of extras included in a computer package usually affects laptop computer prices. Some industry leaders in computer manufacturing make it a selling point to offer computers in sleek styling and in a variety of colors. They may also offer unusual or contemporary system design. Though this is less important to many consumers, for those who do value “looks,” this type of system may be well worth the extra cost.")
     with open('Login_bg.jpg', "rb") as image_file:
